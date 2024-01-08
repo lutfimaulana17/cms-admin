3f248d010d4236f950cf28b2c826bce235419143
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2024-01-08 18:56:50 +0700

    layouting transactions list component

--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,8 +1,62 @@
+import Image from 'next/image'
 import styles from './transactions.module.css'
 
 const Transactions = () => {
   return (
-    <div>Transactions</div>
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image src="/noavatar.png" alt="usrImage" width={40} height={40} className={styles.userImage} />
+                Lutfi Maulana
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>Pending</span>
+            </td>
+            <td>08.01.2024</td>
+            <td>$5.640</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image src="/noavatar.png" alt="usrImage" width={40} height={40} className={styles.userImage} />
+                Lutfi Maulana
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Done</span>
+            </td>
+            <td>08.01.2024</td>
+            <td>$5.640</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image src="/noavatar.png" alt="usrImage" width={40} height={40} className={styles.userImage} />
+                Lutfi Maulana
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>Cancelled</span>
+            </td>
+            <td>08.01.2024</td>
+            <td>$5.640</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 

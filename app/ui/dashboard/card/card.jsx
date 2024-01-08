3d08282a93c35efdd625a658c6caa73aa2c73663
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2024-01-08 13:23:35 +0700

    layouting card component

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,8 +1,18 @@
+import { MdSupervisedUserCircle } from 'react-icons/md'
 import styles from './card.module.css'
 
 const Card = () => {
   return (
-    <div className={styles.container}>Card</div>
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}>123.500</span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>15%</span> more than previous week
+        </span>
+      </div>
+    </div>
   )
 }
 

869a9df33be3d2dae934047e048409622a20a2c0
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2024-01-16 06:56:19 +0700

    layouting add user page

---
+++ app/dashboard/users/add/page.jsx
@@ -0,0 +1,28 @@
+import styles from '@/app/ui/dashboard/users/addUser/addUser.module.css'
+
+const AddUserPage = () => {
+  return (
+    <div className={styles.container}>
+        <form action="" className={styles.form}>
+            <input type="text" name="username" placeholder="username" required />
+            <input type="email" name="email" placeholder="email" required />
+            <input type="password" name="password" placeholder="password" required />
+            <input type="phone" name="phone" placeholder="phone" />
+            <select name="isAdmin" id="isAdmin">
+                <option value={false}>Is Admin?</option>
+                <option value={true}>Yes</option>
+                <option value={false}>No</option>
+            </select>
+            <select name="isActive" id="isActive">
+                <option value={true}>Is Active?</option>
+                <option value={true}>Yes</option>
+                <option value={false}>No</option>
+            </select>
+            <textarea name="address" id="address" rows="16" placeholder="address"></textarea>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default AddUserPage

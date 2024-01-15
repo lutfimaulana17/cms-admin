6f33b92a769e38abc04073124cf0a24fb1e236d3
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2024-01-16 05:22:01 +0700

    layoting products list page and fix menu side bar height

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,6 +1,54 @@
+import styles from '@/app/ui/dashboard/products/products.module.css'
+import Search from '@/app/ui/dashboard/search/search'
+import Link from 'next/link'
+import Image from 'next/image'
+import Pagination from '@/app/ui/dashboard/pagination/pagination'
+
 const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>            
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image src="/noproduct.jpg" alt="product img" width={40} height={40} className={styles.productImage} />
+                Soap
+              </div>
+            </td>
+            <td>Desc</td>            
+            <td>$35</td>
+            <td>16.01.2024</td>
+            <td>15</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>View</button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
   )
 }
 

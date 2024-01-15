108ab4991c18fe671c6270a5ac9028be3bffa3bf
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2024-01-16 05:12:09 +0700

    Layouting user list page

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,6 +1,54 @@
+import styles from '@/app/ui/dashboard/users/users.module.css'
+import Search from '@/app/ui/dashboard/search/search'
+import Link from 'next/link'
+import Image from 'next/image'
+import Pagination from '@/app/ui/dashboard/pagination/pagination'
+
 const UsersPage = () => {
   return (
-    <div>UsersPage</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image src="/noavatar.png" alt="usr img" width={40} height={40} className={styles.userImage} />
+                Lutfi
+              </div>
+            </td>
+            <td>lutfi@gmail.com</td>
+            <td>15.01.2024</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>View</button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
   )
 }
 

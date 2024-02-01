c4d694d604efae868605335a1b6888a7d80eef9b
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2024-02-01 21:08:22 +0700

    add logic handle input search component to params

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,11 +1,24 @@
+"use client"
+
 import { MdSearch } from 'react-icons/md'
 import styles from './search.module.css'
+import { usePathname, useSearchParams, useRouter } from 'next/navigation'
 
 const Search = ({placeholder}) => {
+  const searchParams = useSearchParams()
+  const { replace } = useRouter()
+  const pathname = usePathname()
+
+  const handleSearch = (e) => {
+    const params = new URLSearchParams(searchParams)
+    params.set("q", e.target.value)    
+    replace(`${pathname}?${params}`)
+  }
+
   return (
     <div className={styles.container}>
         <MdSearch />
-        <input type="text" placeholder={placeholder} className={styles.input} />
+        <input type="text" placeholder={placeholder} className={styles.input} onChange={handleSearch} />
     </div>
   )
 }

91c6bc7e46994a6e2a35125d2a41dbf134cf6561
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2024-02-04 20:18:51 +0700

    integration add product with server handle form action

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,11 +1,12 @@
+"use server";
+
 import { revalidatePath } from "next/cache"
-import { User } from "./models"
+import { User, Product } from "./models"
 import { connectToDB } from "./utils"
 import { redirect } from "next/navigation"
 import bcrypt from "bcrypt"
 
 export const addUser = async (formData) => {
-    "use server"
     const { username, email, password, phone, address, isAdmin, isActive } = Object.fromEntries(formData)
     try {
         connectToDB()
@@ -22,4 +23,21 @@ export const addUser = async (formData) => {
 
     revalidatePath("/dashboard/users")
     redirect("/dashboard/users")
+}
+
+export const addProduct = async (formData) => {    
+    const { title, desc, price, stock, color, size } = Object.fromEntries(formData)
+    try {
+        connectToDB()
+        const newProduct = new Product({
+            title, desc, price, stock, color, size
+        })
+        await newProduct.save()
+    } catch (error) {
+        console.log(error)
+        throw new Error("Failed to create product!")
+    }
+
+    revalidatePath("/dashboard/products")
+    redirect("/dashboard/products")
 }

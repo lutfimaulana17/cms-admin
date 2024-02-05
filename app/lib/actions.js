72b9df8582cac773f36fedb8cf88b89067cae4c9
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2024-02-05 09:04:50 +0700

    handle login error and chnage login form to client

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -117,12 +117,15 @@ export const deleteProduct = async (formData) => {
     revalidatePath("/dashboard/products")
 }
 
-export const authenticate = async (formData) => {
-    const { username, password } = Object.fromEntries(formData)
+export const authenticate = async (prevState, formData) => {
+    const { username, password } = Object.fromEntries(formData);
+  
     try {
-        await signIn("credentials", { username, password })
-    } catch (error) {
-        console.log(error)
-        throw error
+      await signIn("credentials", { username, password });
+    } catch (err) {
+      if (err.message.includes("CredentialsSignin")) {
+        return "Wrong Credentials";
+      }
+      throw err;
     }
 }
